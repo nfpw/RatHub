53cbf034817c91ad2e3f8963d1f9d611a73d52e9
nfpw <153939922+nfpw@users.noreply.github.com>
2025-04-12 00:38:24 +0300

    Update Launch.lua

--- Launch.lua
+++ Launch.lua
@@ -1,9 +1,9 @@
 local shared = getgenv() or shared or _G; shared.RatHubLaunch = {
-    Key = nil;
+    Key = "ineedtosleep";
     Library = "DollarWare"; -- DollarWare, Linoria, Luna
     Debug = false;
     Execute = function(string)
-        return loadstring(string, true)();
+        return loadstring(string)();
     end;
 }; if game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled and not game:GetService("UserInputService").MouseEnabled then shared.RatHubLaunch.Library = "Luna"; end;
-shared.RatHubLaunch.Execute("https://raw.githubusercontent.com/nfpw/RatHub/refs/heads/main/Loader.lua");
+shared.RatHubLaunch.Execute(game:HttpGetAsync"https://raw.githubusercontent.com/nfpw/RatHub/refs/heads/main/Loader.lua");
